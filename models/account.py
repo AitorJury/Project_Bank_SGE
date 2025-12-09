aa94ee84771c30dea2293fd0fa93dc5c300d001b
aitor <aitor@2DAM-008>
2025-12-09 11:59:54 +0100

    Modificación Modelo Account

--- models/account.py
+++ models/account.py
@@ -1,18 +1,45 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api
+from odoo import fields
+from odoo import models
 
 
 class Account(models.Model):
     _name = 'g3_bank.account'
     _description = 'Account'
 
-    name = fields.Char()
-    balance = fields.Monetary()
-    creditLine = fields.Monetary(Required = "False")
-    beginBalance = fields.Monetary()
-    beginBalanceTimestamp = fields.Date()
-    typeAccount = fields.selection()
+# Enumeración para el campo typeAccount
+    TYPE_ACCOUNT_SELECTION = [
+        ('STANDARD', 'Standard'),
+        ('CREDIT', 'Credit'),
+    ]
+
+#   El id no se define, Odoo lo añade automáticamente
+#   Utilizo name como la descripción de Account
+    name = fields.Char(string='Description', required=True)
+    balance = fields.Double(string='Balance', required=True)
+    creditLine = fields.Double(string='Credit Line')
+    beginBalance = fields.Double(string='Begin Balance', required=True)
+    beginBalanceTimestamp = fields.Date(string='Begin Balance Timestamp', required=True)
+    # La selección del tipo de cuenta
+    typeAccount = fields.Selection(selection=TYPE_ACCOUNT_SELECTION, string='Account Type', required=True)
+    
+#   Relación con Customer (Muchos a Muchos)
+    customer_ids = fields.Many2many(
+        'g3_bank.customer', 
+        'g3_bank_account_customer_rel', 
+        'account_id', 
+        'customer_id', 
+        string='Customers'
+    )
+#   Relación con Movement (Uno a Muchos)
+    movement_ids = fields.One2many(
+        'g3_bank.movement', 
+        'account_id',
+        string='Movements'
+    )
+
 #
 #     @api.depends('value')
 #     def _value_pc(self):
