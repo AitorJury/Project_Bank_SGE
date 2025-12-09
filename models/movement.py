e7a4bdf46cccdcdc7f2fdc6d557f2263a47ee6c1
cynthia <cynthia16123@gmail.com>
2025-12-09 13:59:28 +0100

    Hecho modelo movement

--- models/movement.py
+++ models/movement.py
@@ -6,8 +6,14 @@ from odoo import models, fields, api
 class Movement(models.Model):
     _name = 'g3_bank.movement'
     _description = 'Movement'
-
-    name = fields.Char()
+    
+    name = fields.Char(string = "Description", required = True)
+    timestamp = fields.DateTime(string = "TimeStamp", required = True)
+    amount = fields.Monetary(String = "Amount", required = True)
+    balance = fields.Monetary(String = "Balance", required = True)
+    
+    account_id = fields.Many2one('g3_bank.account', string="Account", )
+    
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
