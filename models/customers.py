47079144bd41befd9cde0e6205f1231e70bcccd7
JuanICD <juancaizaduenas@gmail.com>
2025-12-09 14:08:36 +0100

    creacion de modulo de customer

--- models/customers.py
+++ models/customers.py
@@ -7,12 +7,17 @@ class Customer(models.Model):
      _name = 'g3_bank.customer'
      _description = 'Customer'
 
-     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     #name equivale al campo descripcion
+     #name = fields.Char()
+     #----------------------
+     first_name = fields.Char(required=True)
+     last_name = fields.Char(required=True)
+     middle_name = fields.Char(required=True)
+     street = fields.Char(required=True)
+     city = fields.Char(required=True)
+     state = fields.Char(required=True)
+     zip = fields.Integer(required=True)
+     phone = fields.Integer(required=True)
+     email = fields.Char(required=True)
+     password = fields.Char(required=True)
+     account_ids=fields.Many2many('g3_bank.account')
