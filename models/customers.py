c95002032b8020fd7b79a268cb2d30c01f5e544c
JuanICD <juancaizaduenas@gmail.com>
2025-12-16 14:01:02 +0100

    herenciav2

--- models/customers.py
+++ models/customers.py
@@ -11,14 +11,5 @@ class Customer(models.Model):
      #name equivale al campo descripcion
      #name = fields.Char()
      #----------------------
-     first_name = fields.Char(required=True)
-     last_name = fields.Char(required=True)
-     middle_name = fields.Char(required=True)
-     street = fields.Char(required=True)
-     city = fields.Char(required=True)
-     state = fields.Char(required=True)
-     zip = fields.Integer(required=True)
-     phone = fields.Integer(required=True)
-     email = fields.Char(required=True)
-     password = fields.Char(required=True)
+     
      account_ids=fields.Many2many('g3_bank.account')
