63eafbb3af025e7d84ee5e569cbe7866907c6dca
cynthia <cynthia16123@gmail.com>
2026-01-09 13:27:42 +0100

    Menu movimiento

--- models/movement.py
+++ models/movement.py
@@ -12,7 +12,7 @@ class Movement(models.Model):
     amount = fields.Monetary(String = "Amount", required = True)
     balance = fields.Monetary(String = "Balance", required = True)
     
-    account_id = fields.Many2one('g3_bank.account', string="Account" )
+    account_id = fields.Many2one('g3_bank.account', string="Account")
     
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
