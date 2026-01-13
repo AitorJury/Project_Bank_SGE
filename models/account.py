4c5dca222f13cdda7fc83c8a07bf4368fe84daa5
aitor <aitor@2DAM-008>
2026-01-13 11:47:39 +0100

    Configuración de detalles de las vistas.

--- models/account.py
+++ models/account.py
@@ -21,9 +21,9 @@ class Account(models.Model):
     ], string='Account Type', required=True, default='STANDARD')
     
 #   Relación con Customer (Muchos a Muchos)
-    customer_ids = fields.Many2many('res.partner', string='Customers')
+    customer_ids = fields.Many2many('res.users', string='Customers')
 #   Relación con Movement (Uno a Muchos)
-    movement_ids = fields.One2many('g3_bank.movement', 'account_id', ondelete='cascade', string='Movements')
+    movement_ids = fields.One2many('g3_bank.movement', 'account_id', string='Movements')
 
 #
 #     @api.depends('value')
