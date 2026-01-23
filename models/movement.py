3aa7b4090626726386969e912c68110478953d5b
JuanICD <juancaizaduenas@gmail.com>
2026-01-23 12:43:33 +0100

    descomentado campo account_id en el modelo movement

--- models/movement.py
+++ models/movement.py
@@ -16,7 +16,7 @@ class Movement(models.Model):
     amount = fields.Monetary(string = "Amount", currency_field='currency_id', default=0.0)
     balance = fields.Monetary(string = "Balance", currency_field='currency_id', default=0.0)
     
-    #account_id = fields.Many2one('g3_bank.account', string="Account")
+    account_id = fields.Many2one('g3_bank.account', string="Account")
     
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
