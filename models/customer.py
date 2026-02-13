d584b5d61eeba46eeb3817a3bfd9ae35de335459
JuanICD <juancaizaduenas@gmail.com>
2026-02-13 11:40:54 +0100

    añadidos nuevos grupos de acceso (Admins y Customers) y ajustes en derechos de acceso; actualización en vistas y modelo customer con dominio para cuentas

--- models/customer.py
+++ models/customer.py
@@ -15,7 +15,6 @@ class Customer(models.Model):
      _inherit = 'res.users'
      account_ids=fields.Many2many('g3_bank.account',
                                   #Solo muestran las cuentas que tengan saldo inicial y no esten asignadas a un cliente
-                                  domain=[('customer_ids', '=', False),('balance','>',0)],
                                   string='Cuentas con Saldo Inicial'
                                   )
 
